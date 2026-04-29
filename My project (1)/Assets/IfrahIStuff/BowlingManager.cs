75dbd374b30ad49762e5853150da3f86943a7309
Aasiya <aasiya.asadullah@gmail.com>
2026-04-29 17:45:18 +0500

    jibsdc

--- My project (1)/Assets/IfrahIStuff/BowlingManager.cs
+++ My project (1)/Assets/IfrahIStuff/BowlingManager.cs
@@ -1,84 +1,84 @@
-using UnityEngine;
-using TMPro;
-using System.Collections;
+// using UnityEngine;
+// using TMPro;
+// using System.Collections;
 
-public class BowlingManager : MonoBehaviour
-{
-    public static BowlingManager instance;
+// public class BowlingManager : MonoBehaviour
+// {
+//     public static BowlingManager instance;
 
-    [Header("UI Text Elements")]
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI triesText;
+//     [Header("UI Text Elements")]
+//     public TextMeshProUGUI scoreText;
+//     public TextMeshProUGUI triesText;
 
-    [Header("Game Rules")]
-    public int score = 0;
-    public int tries = 2;
-    public int totalPins = 10;
+//     [Header("Game Rules")]
+//     public int score = 0;
+//     public int tries = 2;
+//     public int totalPins = 10;
 
-    private bool isResetting = false;
+//     private bool isResetting = false;
 
-    void Awake()
-    {
-        if (instance == null) instance = this;
-    }
+//     void Awake()
+//     {
+//         if (instance == null) instance = this;
+//     }
 
-    void Start()
-    {
-        FullReset();
-    }
+//     void Start()
+//     {
+//         FullReset();
+//     }
 
-    // Called by PinDetector script
-    public void AddScore()
-    {
-        score++;
-        UpdateUI();
+//     // Called by PinDetector script
+//     public void AddScore()
+//     {
+//         score++;
+//         UpdateUI();
 
-        // If they get a Strike (all pins down), reset immediately!
-        if (score >= totalPins && !isResetting)
-        {
-            StartCoroutine(AutoResetTimer());
-        }
-    }
+//         // If they get a Strike (all pins down), reset immediately!
+//         if (score >= totalPins && !isResetting)
+//         {
+//             StartCoroutine(AutoResetTimer());
+//         }
+//     }
 
-    // This is called by the "BallPitTrigger" when a ball is thrown
-    public void BallThrown()
-    {
-        if (isResetting) return;
+//     // This is called by the "BallPitTrigger" when a ball is thrown
+//     public void BallThrown()
+//     {
+//         if (isResetting) return;
 
-        tries--;
-        UpdateUI();
+//         tries--;
+//         UpdateUI();
 
-        if (tries <= 0 && !isResetting)
-        {
-            StartCoroutine(AutoResetTimer());
-        }
-    }
+//         if (tries <= 0 && !isResetting)
+//         {
+//             StartCoroutine(AutoResetTimer());
+//         }
+//     }
 
-    void UpdateUI()
-    {
-        if (scoreText != null) scoreText.text = "Score: " + score;
-        if (triesText != null) triesText.text = "Tries: " + tries;
-    }
+//     void UpdateUI()
+//     {
+//         if (scoreText != null) scoreText.text = "Score: " + score;
+//         if (triesText != null) triesText.text = "Tries: " + tries;
+//     }
 
-    IEnumerator AutoResetTimer()
-    {
-        isResetting = true;
-        yield return new WaitForSeconds(5f); /
-        FullReset();
-        isResetting = false;
-    }
+//     IEnumerator AutoResetTimer()
+//     {
+//         isResetting = true;
+//         yield return new WaitForSeconds(5f); 
+//         FullReset();
+//         isResetting = false;
+//     }
 
-    public void FullReset()
-    {
-        score = 0;
-        tries = 2;
-        UpdateUI();
+//     public void FullReset()
+//     {
+//         score = 0;
+//         tries = 2;
+//         UpdateUI();
 
-        // Reset all pins to their starting spots
-        PinDetector[] allPins = FindObjectsOfType<PinDetector>();
-        foreach (PinDetector pin in allPins)
-        {
-            pin.ResetPin();
-        }
-    }
-}
+//         // Reset all pins to their starting spots
+//         PinDetector[] allPins = FindObjectsOfType<PinDetector>();
+//         foreach (PinDetector pin in allPins)
+//         {
+//             pin.ResetPin();
+//         }
+//     }
+// }
